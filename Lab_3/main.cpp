5c70fa5aad7ca67b69226f2e3dd28117db04c48c
Kirill Brotskiy <brotskia_26@mail.ru>
2020-03-07 08:44:52 +0300

    Initial Commit

---
+++ Lab_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab_3
+//
+//  Created by Кирилл Броцкий on 07.03.2020.
+//  Copyright © 2020 Кирилл Броцкий. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
