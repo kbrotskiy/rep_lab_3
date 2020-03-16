26535eab1e6762bb9a66099801c566c542390b30
Kirill Brotskiy <56027225+kbrotskiy@users.noreply.github.com>
2020-03-17 02:23:33 +0300

    Update main.cpp

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -9,18 +9,23 @@
 #include <iostream>
 #include <cmath>
 #include "Class.h"
- 
 using namespace std;
  
 int main()
 {
-    int x1, y1;
-    cout << "Vvedite koordinatu X: x="; cin >> x1;
-    cout << "Vvedite koordinatu Y: y="; cin >> y1;
-    vector_na_ploskosti A(x1,y1);
-    vector_na_ploskosti *pA;
-    pA=&A;
+    int x1, y1, z1 = 0, c;
+    cout<<"Vvedite tsifry prostranstvo dvuxmernoe ili trexmernoe (2/3) ->";
+    cin>>c;
+    cout << "Vvedite koordinatu X: x="; cin >> x1; cout << endl;
+    cout << "Vvedite koordinatu Y: y="; cin >> y1; cout << endl;
+    if (c==3) { cout << "Vvedite koordinatu Z: z="; cin >> z1; cout << endl; }
+    Vect2D A(x1,y1);
+    Vect3D B(x1,y1,z1);
+    Vect2D *pA;
+    if(c==2)
+        pA=&A;
+    else
+        pA=&B;
     pA->print();
-    cout<<endl;
     return 0;
 }
